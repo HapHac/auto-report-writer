095655f16018f9ab31c3a68f9e95e6e91bf56f4c
HapHac <37301867+HapHac@users.noreply.github.com>
2018-12-14 13:18:48 +0800

    Update trs2rep.pl

--- trs2rep.pl
+++ trs2rep.pl
@@ -2,6 +2,6 @@ undef $/;
 $test=<>;
 unless($test=~m/^#Information Of You/)
 {$test=~s/^/#Information Of You\n/g;}
-$test=~s/^/```python\n/;
+$test=~s/^/```\n/;
 $test=~s/$/\n```\n/;
 print ($test);
