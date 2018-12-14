cfd16cd79e2cd848b2bb6331a647ef02124f7b61
HapHac <37301867+HapHac@users.noreply.github.com>
2018-12-14 13:42:10 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,5 @@
 @echo ## 实验报告 >./out/out.md
 @echo off
-:: ::set i=0
-:: for /F "tokens=4 skip=7 usebackq" %%i in (*.py) do (
-:: 	if (%%i in *.py) @echo %%i)
 for %%i IN (./in/*.py) DO (
 	cd .>./out/%%i.JPG
 	add.bat %%i)
